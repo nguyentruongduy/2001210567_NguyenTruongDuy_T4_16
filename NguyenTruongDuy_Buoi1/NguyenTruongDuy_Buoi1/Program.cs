0520d9f3cdc8adc3f8730eede873b19e8474b4bd
Truong Duy <tduy17112003@gmail.com>
2024-07-10 10:55:32 +0700

    Tong Mang

---
+++ NguyenTruongDuy_Buoi1/NguyenTruongDuy_Buoi1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace NguyenTruongDuy_Buoi1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0;
+            int[] arr = new int[] { 1, 2, 4, 5, 6, 7 };
+            for (int i = 0; i < arr.Length; i++)
+            {
+                a = a + arr[i];
+            }
+            Console.Write("Tổng mảng la :" + a);
+            Console.ReadKey();
+        }
+
+       
+            
+       
+    }
+}
