1d110c97bfc0f146ca5a1c189bf262bddc30fed4
Truong Duy <tduy17112003@gmail.com>
2024-07-16 14:50:31 +0700

    Nhân hai số nguyên lớn

---
+++ NguyenTruongDuy_Buoi1/NhanHaiSoNguyenLon/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NhanHaiSoNguyenLon
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Khai báo hai số nguyên lớn
+            BigInteger num1 = BigInteger.Parse("123456789012345678901234567890");
+
+            BigInteger num2 = BigInteger.Parse("987654321098765432109876543210");
+
+            // Nhân hai số nguyên lớn
+            BigInteger result = num1 * num2;
+
+            // In kết quả
+            Console.WriteLine("Kết quả của phép nhân là: " + result);
+            Console.ReadKey();
+        }
+    }
+}
